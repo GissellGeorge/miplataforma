4e4ba9ccf4cedf0e83ac9cd0b7f8f3ea0c35445c
GissellGeorge <gissellgeorge@gmail.com>
2022-09-10 14:16:52 -0500

    base de datos

---
+++ datos.sql
@@ -0,0 +1,5 @@
+Create database miplataforma;
+Use miplataforma;
+
+Create table persona (id persona int, nombres varchar(20), apellidos varchar(20),cedula varchar(10),id genero int,fechanacimiento date, id nacionalidad int, id etnia int,id estado civil int);
+
